839732333f51c4e4072bdd1d9fe99ff150ba20f7
Leuug <leuugz@gmail.com>
2023-03-21 11:40:31 -0300

    Just a few refactorings before addig new features
    I know I prommissed better commit messages, sorry :P

--- src/Game.gd
+++ src/Game.gd
@@ -1,16 +1,22 @@
 extends Node
 
-var oponent_move = false
-var ball_initial_pos = "player"
-var ball_pos = 0
-var player_pos = 0
-var oponent_pos = 0
-var player_score = 0
-var oponent_score = 0
-var default_speed = 400
-var ball_speed = 700
+var oponent_move: bool = false
+var ball_initial_pos: String = "player"
+var ball_pos: float = 0
+var player_pos: Vector2 = Vector2.ZERO
+var oponent_pos: Vector2 = Vector2.ZERO
+var player_score: int = 0
+var oponent_score: int = 0
+var default_speed: int = 400
+var ball_speed: int = 700
 
 
-func _input(_event):
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("fullscreen"):
-		get_window().mode = Window.MODE_EXCLUSIVE_FULLSCREEN if (!((get_window().mode == Window.MODE_EXCLUSIVE_FULLSCREEN) or (get_window().mode == Window.MODE_FULLSCREEN))) else Window.MODE_WINDOWED
+		if get_window().mode == Window.MODE_WINDOWED:
+			if OS.get_name() == "Windows":
+				get_window().mode = (Window.MODE_EXCLUSIVE_FULLSCREEN)
+			else:
+				get_window().mode = (Window.MODE_FULLSCREEN)
+		else:
+			get_window().mode = (Window.MODE_WINDOWED)
--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
 
-var speed
-var direction = Vector2()
+var direction: Vector2
+var collision: KinematicCollision2D
 
-func _ready():
-	speed = Game.default_speed
-	global_position.y = 720.0/2.0
+@onready var speed: int = Game.default_speed
+
+func _ready() -> void:
+	global_position.y = DisplayServer.screen_get_size().y / 2.0
 	Game.player_pos = $Marker2D.global_position
 
-func _input(_event):
+
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_pressed("player_up"):
 		direction.y = -1
 	elif Input.is_action_pressed("player_down"):
@@ -16,5 +18,6 @@ func _input(_event):
 	else:
 		direction.y = 0
 
-func _physics_process(delta):
-	var _collision = move_and_collide(direction * speed * delta)
+
+func _physics_process(delta: float) -> void:
+	collision = move_and_collide(direction * speed * delta)
