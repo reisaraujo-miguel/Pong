b5ebe31a140db46bca792e83c1a648bd61e4b765
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2023-03-25 22:56:31 -0300

    Formatting
    Just a few formatting asjustments

--- src/Score/Score.gd
+++ src/Score/Score.gd
@@ -1,8 +1,10 @@
 extends Control
 
+
 func _ready() -> void:
 	$ReferenceRect/Player_Score.text = str(Game.player_score)
 	$ReferenceRect/Oponent_Score.text = str(Game.oponent_score)
+
 
 func _on_score_area_player_scored() -> void:
 	$ReferenceRect/Player_Score.text = str(Game.player_score)
