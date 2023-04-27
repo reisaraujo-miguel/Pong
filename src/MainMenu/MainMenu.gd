e13a93aa64f4bdd4172a1a75f2e864d4c61a9d86
Leuug <leuugz@gmail.com>
2023-04-27 12:43:49 -0300

    New Load Scene
    I changed the way scene chnages are handled so that I can use the `Game`
    sigleton to check when `Esc` is pressed and change the current scene to
    the Main scene at any point in the game.
    
    On the traditional way this can't be done because the `Game`secene and
    the current game scene are on different trees, so I can't access it
    through `get_tree()`

--- src/MainMenu/MainMenu.gd
+++ src/MainMenu/MainMenu.gd
@@ -2,9 +2,7 @@ extends Control
 
 
 func _on_start_btn_button_up() -> void:
-	var _new_scene: Error = get_tree().change_scene_to_file(
-		"res://src/ModeMenu/ModeMenu.tscn"
-	)
+	Game.goto_scene("res://src/ModeMenu/ModeMenu.tscn")
 
 
 func _on_quit_btn_button_up() -> void:
