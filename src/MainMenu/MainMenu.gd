67382190de8ea7ebe1857e793bb4320a2fd5b5b0
Leuug <leuugz@gmail.com>
2023-04-27 10:29:10 -0300

    Add Main Menu Scene

---
+++ src/MainMenu/MainMenu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _on_start_btn_button_up() -> void:
+	var _new_scene: Error = get_tree().change_scene_to_file(
+		"res://src/Match/Match.tscn"
+	)
+
+
+func _on_quit_btn_button_up() -> void:
+	get_tree().quit()
