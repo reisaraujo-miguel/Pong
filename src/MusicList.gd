071d147c851319f35065923a388955dae57db7a2
Leuug <leuugz@gmail.com>
2023-05-27 22:12:22 -0300

    Nothing to see here...

--- src/MusicList.gd
+++ src/MusicList.gd
@@ -1,18 +1,27 @@
 extends Node
 
-var curr_song: int = 0;
-var total_songs: int = 0;
+var curr_song: int = 0
+var total_songs: int = 0
+var is_legal: bool = false
 
 var list: Array
 
 func _ready() -> void:
-	var dir: DirAccess = DirAccess.open("res://assets/sounds/musics/")
+	var path: String
+
+	if is_legal:
+		path = "res://assets/sounds/musics/"
+	else:
+		path = "res://assets/sounds/illegal/"
+
+	var dir: DirAccess = DirAccess.open(path)
+
 	dir.list_dir_begin()
 
 	var file = dir.get_next()
 	while file != "":
 		if (not file.begins_with(".")) and (not file.ends_with(".import")):
-			list.append(load("res://assets/sounds/musics/" + file))
+			list.append(load(path + file))
 
 		file = dir.get_next()
 
